655fb0e032282df6236aff29f7d047f8a75ad9af
Etuk Mfon <emfon71@gmail.com>
2020-11-03 08:18:43 +0100

    still testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,8 +52,8 @@ exports.logIn = async (req, res, next) => {
     const user = await User.findOne({email}).select('+password')
 
     console.log(req.get('host'))
-    console.log(req.originalUrl)
-    console.log(req.url)
+    console.log(res)
+    console.log(res.request)
 
     if (!user || !await user.verifyPassword(password, user.password)) {
       return next(new customError('Invalid email or password', 401))
