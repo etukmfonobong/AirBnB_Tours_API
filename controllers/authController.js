357aede399f9c9c0cebd6ee99b8fbfc2d1b4b21b
Etuk Mfon <emfon71@gmail.com>
2020-11-03 21:03:38 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,6 +83,7 @@ exports.secureToken = async (req, res, next) => {
     res.cookie('jwtsig', signature, {
       expires: farFuture,
       // expiresIn: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+      domain: '.airbnb-tours-etukmfon.herokuapp.com',
       secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
       path: '/',
       sameSite: sameSite,
@@ -92,6 +93,7 @@ exports.secureToken = async (req, res, next) => {
     res.cookie('jwthandp', headerToken, {
       expires: farFuture,
       // expiresIn: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+      domain: '.airbnb-tours-etukmfon.herokuapp.com',
       secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
       path: '/',
       sameSite: sameSite,
